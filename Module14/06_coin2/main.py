5c3be99f2b67ab5741ac9a7b99f0f97cab612534
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-03 20:20:46 +0300

    Done 6

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+from math import sqrt
+
+print('Введите координаты монетки: ')
+x = float(input('Коорд-та x: '))
+y = float(input('Коорд-та y: '))
+r = float(input('Введите радиус: '))
+
+distance_fron_zero = sqrt(x ** 2 + y ** 2)
+
+def find(distance,radius):
+  if distance_fron_zero <= r:
+    print('Монетка где-то рядом')
+  else:
+    print('Монетки в области нет')  
+
+find(distance_fron_zero, r)

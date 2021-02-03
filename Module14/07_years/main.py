d09efa436b8915b8ff984d298a31fb2612089328
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-03 20:48:45 +0300

    Done 7

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+a = int(input('Введите первый год: '))
+b = int(input('Введите второй год: '))
+
+for year in range(a, b + 1):
+    same_year = year
+    for number in range(0, 10):
+        year = same_year
+        count = 0
+        while year != 0:
+            if year % 10 == number:
+                count += 1
+            year //= 10
+        if count == 3:
+            print(same_year)

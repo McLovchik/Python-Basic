8dd9cc2b81c5e556aa66195c57e8e8eba4f2269d
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-04 22:13:47 +0300

    Done 3*

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,11 +1,12 @@
 number = int(input('Введите число: '))
 
-# TODO: До и после функции должно быть по 2 пустые строки;)
+
 def summ(number):
     summa = 0
     for i in range(1, number + 1):
         summa += i
     return summa
+
 
 def sum_number(number):
     summa_number = 0
@@ -14,12 +15,14 @@ def sum_number(number):
         number //= 10
     return summa_number
 
+
 def numbers(number):
     count = 0
     while number != 0:
         count += 1
         number //= 10
     return count
+
 
 print('По условию задачи')
 print('Сумма чисел:', summ(number))
@@ -30,7 +33,6 @@ print()
 print('===================')
 print()
 
-# TODO: В конце файла должна быть одна пустая строка:)
 print('По примеру работы программы')
 print('Сумма чисел:', sum_number(number))
 print('Кол-во цифр в числе:', numbers(number))

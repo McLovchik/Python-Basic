8f85cf87a3984624ab04e2a8b2aa133b24d17b37
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-04 22:15:24 +0300

    Done 5*

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,10 +1,11 @@
 number = int(input('Введите число: '))
 
-# TODO: Аналогично 3:)
+
 def min_divider(number):
     for i in range(number, 1, -1):
         if number % i == 0:
             divider = i
     return divider
+
 
 print('Наименьший делитель, отличный от единицы:',min_divider(number))

0f2ef163b303d904a264a3f08ea5f85ca223b6b1
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-04 22:20:37 +0300

    Done 6*

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -5,16 +5,14 @@ x = float(input('Коорд-та x: '))
 y = float(input('Коорд-та y: '))
 r = float(input('Введите радиус: '))
 
-# TODO: Опечатка;)
-distance_fron_zero = sqrt(x ** 2 + y ** 2)
+distance_from_zero = sqrt(x ** 2 + y ** 2)
 
-# TODO: Аналогично 3:)
-def find(distance,radius):
-  # TODO: 1) В функцию передаются элементы, которые в итоге не используются:(
-  #  2) Не хватает пробелов;)
-  if distance_fron_zero <= r:
-    print('Монетка где-то рядом')
-  else:
-    print('Монетки в области нет')  
 
-find(distance_fron_zero, r)
+def find(distance, radius):
+    if distance <= radius:
+        print('Монетка где-то рядом')
+    else:
+        print('Монетки в области нет')
+
+
+find(distance_from_zero, r)

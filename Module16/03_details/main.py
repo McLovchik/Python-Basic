9b722a5534accbae50472a69e10a4abc3954750a
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-25 13:38:42 +0300

    16.3

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,14 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+detail = input('Название детали: ')
+summa_money = 0
+summa_det = 0
+
+for i_index in range(0, len(shop)):
+        if shop[i_index][0] == detail:
+                summa_money += shop[i_index][1]
+                summa_det += 1
+
+print('Кол-во деталей -', summa_det)
+print('Общая стоимость -', summa_money)

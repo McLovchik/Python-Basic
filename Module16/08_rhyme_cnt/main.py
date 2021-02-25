57ad085ea4ab1998f2a7d9cfcdc139a56dc069d8
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-25 13:43:12 +0300

    16.8

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+people = int(input('Кол-во человек: '))
+people_list = list(range(1, people + 1))
+drop_out = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый', drop_out, 'человек')
+print()
+
+start = 0
+while len(people_list) != 1:
+    print('Текущий круг людей:', people_list)
+    print('Начало счёта с номера', people_list[start])
+    retired = drop_out - len(people_list) - 1 + start
+    while retired >= len(people_list):
+        retired -= len(people_list)
+    print('Выбывает человек под номером', people_list[retired])
+    people_list.remove(people_list[retired])
+    start = retired
+    while start == len(people_list):
+        start = 0
+    print()
+
+print('Остался человек под номером', people_list[0])

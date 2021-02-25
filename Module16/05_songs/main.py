0233204d564d54d29f601f90f0d8ecda5e03c111
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-25 13:42:01 +0300

    16.5

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -10,4 +10,16 @@ violator_songs = [
     ['Clean', 5.83]
 ]
 
-# TODO здесь писать код
+special_playlist = []
+
+songs = int(input('Сколько песен выбрать? '))
+time = 0
+
+for i_num in range(1, songs + 1):
+    song = input(f'Название {i_num} песни: ')
+    for i_index in range(0, len(violator_songs)):
+        if violator_songs[i_index][0] == song:
+            time += violator_songs[i_index][1]
+
+print()
+print('Общее время звучания песен: ', time, 'минут')

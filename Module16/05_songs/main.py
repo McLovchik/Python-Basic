48f1d9d1e44976556a65e5bbdf127443a0fefade
aleksandr_ignatev <aleksandrignatev@bk.ru>
2021-02-26 19:33:11 +0300

    16.5 *

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -17,7 +17,7 @@ time = 0
 
 for i_num in range(1, songs + 1):
     song = input(f'Название {i_num} песни: ')
-    for i_index in range(0, len(violator_songs)):
+    for i_index in violator_songs:  # Так не работает
         if violator_songs[i_index][0] == song:
             time += violator_songs[i_index][1]
 

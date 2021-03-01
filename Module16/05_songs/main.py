07eca445919c978872c645360b41f8be5b07753b
vdstr <vd@stratov.ru>
2021-03-01 20:20:47 +0300

    module 16 rework accepted 1/5

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -17,6 +17,7 @@ time = 0
 
 for i_num in range(1, songs + 1):
     song = input(f'Название {i_num} песни: ')
+    # TODO: Аналогично 3;)
     for index in violator_songs:
         if index[0] == song:
             time += index[1]

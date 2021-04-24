8412f9679039097a97ae1523543977a7ff44505f
McLovchik <aleksandrignatev@bk.ru>
2021-04-25 00:27:12 +0300

    18 last_fix

--- Module18/09_message/main.py
+++ Module18/09_message/main.py
@@ -1,3 +1,9 @@
+def fun_if_part_of_word_is_not_alpha(fun_word, fun_begin_index, fun_i_symbol_index):
+    fun_var_word = word[fun_begin_index:fun_i_symbol_index][::-1]
+    fun_var_word = ''.join([fun_var_word, fun_word[fun_i_symbol_index]])
+    return fun_var_word
+
+
 message = input('Сообщение: ')
 message_split = message.split()
 
@@ -12,19 +18,12 @@ for word in message_split:
                 var_full_word.append((word[begin_index:len(word)])[::-1])
                 break
             elif not word[i_symbol_index].isalpha():
-                var_word = word[begin_index:i_symbol_index]
-                var_word = var_word[::-1]
-                var_word = ''.join([var_word, word[i_symbol_index]])
-                var_full_word.append(var_word)
+                # var_word = word[begin_index:i_symbol_index][::-1]
+                # var_word = ''.join([var_word, word[i_symbol_index]])
+                var_full_word.append(fun_if_part_of_word_is_not_alpha(word, begin_index, i_symbol_index))
                 begin_index = i_symbol_index + 1
         new_message_split.append(''.join(var_full_word))
     else:
         new_message_split.append(word[::-1])
 
 print(' '.join(new_message_split))
-
-# Сейчас кажется понятнее, а если делить на функции, то не очень удобно же, придется несколько переменных сувать
-# в функцию
-# TODO: А в чем собственно проблема?:)
-#  Несколько аргументов - это нормально. А вот нечитабельный код, увы, нет:(
-#  Попробуйте содержимое elif декомпозировать в ф-ию с информативным именем;)
